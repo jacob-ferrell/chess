a29118ed8dcbca73856bb719ef846ee9b34441f6
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-06-06 16:29:04 -0400

    rewrote methods for determing if game is a draw which now function properly

--- lib/round.rb
+++ lib/round.rb
@@ -7,36 +7,33 @@ class Round
     play_round
     @winner = get_winner 
   end
-
+  #start a new turn for each player until a draw or checkmate
   def play_round
-    @@players.each { |player| Turn.new(player, @board, @@players) unless @board.game_over || is_draw? } until @board.game_over || is_draw?
+    @@players.each { |player| Turn.new(player, @board, @@players) unless end_game? } until end_game?
+    draw unless get_winner
   end
-
+  #test if there is a draw or checkmate
+  def end_game?
+    @board.game_over || is_draw?
+  end
+  #return nil if no player is in checkmate or game is a draw. otherwise return whichever player is not the loser
   def get_winner
     return nil if is_draw? || @@players.select { |player| player.check_mate? }.empty?
     @@players.reject { |player| player == @board.loser }.first
   end
-
+  #determine if there are only kings left or one player has only a king and the other has a king and a bishop or knight
   def is_draw?
-    only_king_left = 0
-    king_and_bishop_or_knight = 0
-    @@players.each do |player|
-      pieces = player.pieces_on_board
-      only_king_left += 1 if pieces.length == 1
-      king_and_bishop_or_knight += 1 if king_and_bishop_or_knight?(pieces)
-    end
-    if only_king_left == 2 || (only_king_left == 1 && king_and_bishop_or_knight == 1)
+    (player1_pieces, player2_pieces) = @@players.first.get_partitioned_pieces
+    total_pieces = player1_pieces.length + player2_pieces.length
+    knight_or_bishop = (player1_pieces + player2_pieces).count { |x| x.is_a?(Knight) || x.is_a?(Bishop) }
+    if total_pieces == 2 || (total_pieces == 3 && knight_or_bishop > 0)
       @board.game_over = true
       return true
     end
-    false
   end
-
-  def king_and_bishop_or_knight?(pieces)
-    pieces = pieces.map { |piece| piece.class }
-    if pieces.length == 2 
-      pieces.include?('Bishop') || pieces.include?('Knight')
-    end
-    false
+  
+  def draw
+    @board.print_board
+    puts "\nIt's a draw! Game over!"
   end
 end
